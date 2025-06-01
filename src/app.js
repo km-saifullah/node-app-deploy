608d351ba83239cb0ef5cc20e1b2dfc3f76f721d
km-saifullah <kmsaifullah16@gmail.com>
2025-06-01 20:24:41 +0600

    root route added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  return res
+    .status(200)
+    .json({ status: true, message: "welcome to the test api" });
+});
 app.use("/api", helloRouter);
 
 export default app;
